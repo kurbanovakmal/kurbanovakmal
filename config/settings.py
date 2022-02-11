f025f5b309c1ca06f2c81a67480d3964833d0ba4
kurbanovakmal <akmal_kurbanov_1997@mail.ru>
2022-02-11 14:28:36 +0300

    herokuga uzgariwlar

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kbmbn#y$1@%ivo(jj=&49we6c4#9k54_3@@-t#rsgf(94)3+#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
